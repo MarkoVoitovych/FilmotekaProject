3784cd0ec5794df0d305b38f703e2e611249a612
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-11-25 03:53:19 +0200

    add index

--- src/index.js
+++ src/index.js
@@ -5,3 +5,18 @@ import { set, get, remove, clear } from './javascript/localStorageUse';
 import { refs } from './javascript/refs';
 import { renderMarkup } from './javascript/renderMarkup';
 import { ThemoviedbAPI } from './javascript/themoviedbAPI';
+
+const themoviedbAPI = new ThemoviedbAPI();
+async function fetch() {
+  const data1 = await themoviedbAPI.fetchFavouritesMovies();
+  //   const data2 = await themoviedbAPI.fetchMovieById();
+  const data3 = await themoviedbAPI.fetchMoviesByQuery();
+  console.log(data1);
+  //   console.log(data2);
+  console.log(data3);
+}
+try {
+  fetch();
+} catch (error) {
+  console.log(error);
+}
