3b9ba0f6f067e4ea6ae860ca355b5640339e136e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-11-27 01:53:25 +0200

    add scripts for spinner, theme, empty gallery-library, smooth scroll

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ try {
 
 export let allProducts = null;
 
-
 async function startPage() {
   const genresIds = await themoviedbAPI.fetchGenres();
   const trendMovies = await themoviedbAPI.fetchFavouritesMovies();
@@ -45,9 +44,7 @@ async function startPage() {
   refs.gallery.innerHTML = markup;
 }
 
-
 //  HEADER
-
 
 const onSearchFormSubmit = async event => {
   event.preventDefault();
@@ -57,10 +54,137 @@ const onSearchFormSubmit = async event => {
     const searchMovies = await themoviedbAPI.fetchMoviesByQuery();
     const markup = searchMovies.results.map(renderMarkup).join('');
     refs.gallery.innerHTML = markup;
-  }
-  catch (err) {
-    console.log(err)
+  } catch (err) {
+    console.log(err);
   }
   event.target.reset();
-}
+};
 refs.formEl.addEventListener('submit', onSearchFormSubmit);
+
+// SPINNER
+function spinnerPlay() {
+  document.querySelector('body').classList.add('loading');
+}
+
+function spinnerStop() {
+  window.setTimeout(function () {
+    document.querySelector('body').classList.remove('loading');
+    document.querySelector('body').classList.add('loaded');
+  }, 1500);
+}
+
+spinnerPlay();
+
+window.addEventListener('load', () => {
+  console.log('All resources finished loading!');
+
+  spinnerStop();
+});
+
+//THEME
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const THEME_STORAGE_KEY = 'theme';
+const inputRef = document.querySelector('.theme-switch__toggle');
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const initPage = () => {
+  const savedChecked = load(THEME_STORAGE_KEY);
+  inputRef.checked = savedChecked;
+  document.body.className = savedChecked ? Theme.DARK : Theme.LIGHT;
+};
+
+initPage();
+
+const onThemeSwitch = event => {
+  const { checked } = event.target;
+
+  document.body.className = checked ? Theme.DARK : Theme.LIGHT;
+  save(THEME_STORAGE_KEY, checked);
+};
+
+inputRef.addEventListener('change', onThemeSwitch);
+
+// SCROLL
+
+const btnUp = document.getElementById('to-top-btn');
+const btnUpWrapper = document.querySelector('.btn-up');
+
+function scrollPage() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+window.addEventListener('scroll', () => {
+  scrollFunction();
+});
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    btnUpWrapper.style.display = 'flex';
+  } else {
+    btnUpWrapper.style.display = 'none';
+  }
+}
+btnUp.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+// EMPTY GALLERY
+const main = document.querySelector('.main-container--gallery');
+
+export default function getWatched() {
+  const fromLSWatched = localStorage.getItem('watched');
+
+  // clearMain();
+  if (fromLSWatched === '[]' || fromLS === null) {
+    clearMain();
+    main?.classList.add('.perspective');
+    return refs.main.insertAdjacentHTML(
+      'afterbegin',
+      `<h1 class="js-title-queue preserve">Your list is empty...</h1>
+      <img src="./images/movie.png" alt="cinema" />
+      <div class="cloak__wrapper preserve">
+        <div class="cloak__container preserve">
+          <div class="cloak preserve"></div>
+        </div>
+      </div>`
+    );
+  }
+  main?.classList.remove('.perspective');
+  // clearMain();
+  const arrayFilms = JSON.parse(fromLSWatched);
+  arrayFilms.reverse();
+  renderMarkup(arrayFilms);
+}
+
+function clearMain() {
+  main.innerHTML = ' ';
+}
