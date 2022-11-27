c47d5120ba7f25cd05a3a97ca154acee7bbc13a8
Andrii Shpak <SHPAKK2@GMAIL.COM>
2022-11-28 00:24:50 +0200

    Removelistener

--- src/javascript/footerModal.js
+++ src/javascript/footerModal.js
@@ -1,14 +1,15 @@
-// import { refs } from './refs';
+import { refs } from './refs';
 
-// refs.footerLink.addEventListener("click", callfooterModal)
+refs.footerLink.addEventListener("click", callfooterModal)
+function callfooterModal() {
+    refs.footerBackDrop.classList.remove('is-hidden');
+    document.addEventListener('keydown', hideFooterModal);
+    refs.footerBackDrop.addEventListener("click", hideFooterModal)
+}
 
-// function callfooterModal() {
-//     refs.footerBackDrop.classList.remove('is-hidden');
-//     document.addEventListener('keydown', function (e) {
-//         console.log(e);
-//         if (e.key === 'Escape') {
-//             refs.footerBackDrop.classList.add('is-hidden');
-//         }
-//     });
-// }
-
+function hideFooterModal(e) {
+  if (e.key === 'Escape' || e.target === refs.footerBackDrop) {
+      refs.footerBackDrop.classList.add('is-hidden');
+        document.removeEventListener('keydown', hideFooterModal)
+    }
+}
