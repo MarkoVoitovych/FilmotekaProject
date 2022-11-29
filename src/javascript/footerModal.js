1a0c2a59da10d9249fc3649cd2a5d7c8ff4b7689
Andrii Shpak <SHPAKK2@GMAIL.COM>
2022-11-29 16:59:27 +0200

    someChanges

--- src/javascript/footerModal.js
+++ src/javascript/footerModal.js
@@ -1,7 +1,12 @@
 import { refs } from './refs';
 
+// console.log(body)
+
+const noScroll = document.querySelector("body")
+
 export function callfooterModal() {
     refs.footerBackDrop.classList.remove('is-hidden');
+    noScroll.classList.add('noScroll')
     document.addEventListener('keydown', hideFooterModal);
     refs.footerBackDrop.addEventListener("click", hideFooterModal)
     refs.footerCloseBtn.addEventListener("click", footerCloseBtnOnClick
@@ -24,4 +29,6 @@ function removeListeners() {
     document.removeEventListener('keydown', hideFooterModal)
     refs.footerBackDrop.removeEventListener("click", hideFooterModal)
     refs.footerCloseBtn.removeEventListener("click", footerCloseBtnOnClick)
+    noScroll.classList.remove('noScroll')
+
 }
